8b6a3a8bbf70934e04b5b4d30c17c28daf79827b
Cuchi <42467470+Cu-chi@users.noreply.github.com>
2021-09-27 20:48:58 +0200

    switching to root folder

--- docs/js/downloads-TotalAndLatestBuild.js
+++
@@ -1,3 +0,0 @@
-var buttons = document.getElementsByClassName("downloadable");
-document.getElementById("versions-available").textContent = buttons.length;
-document.getElementById("latest-version").textContent = buttons[0].getElementsByTagName('span')[0].textContent.replace("(", "").replace(")", "");
